46a4168dc1773c82c469a6f89fa9cdba4ae2b8c8
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-01-10 17:34:05 -0300

    inserido o botão do form

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,15 +4,26 @@ export default class Form extends Component {
   render() {
     return (
       <form>
+        <h3>Login</h3>
         <label htmlFor="emailLogin">
           <input
             type="select"
             placeholder="Email"
-            name="emailLogin"
-            id="emailInput"
+            id="emailLogin"
             data-testid="email-input"
           />
         </label>
+        <label htmlFor="passwordLogin">
+          <input
+            type="password"
+            id="passwordLogin"
+            placeholder="password"
+            data-testid="password-input"
+          />
+        </label>
+        <button type="submit">
+          Entrar
+        </button>
       </form>
     );
   }
