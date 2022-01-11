86d6b7e81773d56cd668e18c4d62f75c7c20f98b
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-01-11 12:04:13 -0300

    correções retirando o comp Form, trazendo pra login. Erealização das verificações de email e senha

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,71 @@
 import React from 'react';
-import Form from '../components/Form';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      isBtnDisabled: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => this.enableBtn());
+  }
+
+  enableBtn = () => {
+    const { email, password } = this.state;
+    const numberOfCharacters = 6;
+    const checkEmail = /\S+@\S+\.\S+/;
+    const emailCondition = checkEmail.test(email);
+    if (emailCondition === true && password.length >= numberOfCharacters) {
+      this.setState({
+        isBtnDisabled: false,
+      });
+    } else {
+      this.setState({
+        isBtnDisabled: true,
+      });
+    }
+  }
+
   render() {
+    const { isBtnDisabled, email, password } = this.state;
+    const { handleChange } = this;
     return (
-      // <>
-      <Form />
-      // </>
+      <form>
+        <h3>Login</h3>
+        <label htmlFor="emailLogin">
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            id="emailLogin"
+            data-testid="email-input"
+            value={ email }
+            onChange={ handleChange }
+          />
+        </label>
+        <label htmlFor="passwordLogin">
+          <input
+            type="password"
+            name="password"
+            id="passwordLogin"
+            placeholder="password"
+            data-testid="password-input"
+            value={ password }
+            onChange={ handleChange }
+          />
+        </label>
+        <button
+          type="submit"
+          disabled={ isBtnDisabled }
+        >
+          Entrar
+        </button>
+      </form>
     );
   }
 }
